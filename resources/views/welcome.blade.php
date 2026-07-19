2e97c5e5ff856d76eed0ad66720a57edc2c8757c
Root4tech <usman@root4tech.com>
2026-07-19 14:04:38 +0500

    Mass Update 5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'QuotationSystem') }} - Professional Quotation Management</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/app.css">
+    <script defer src="/js/app.js"></script>
     <style>
         :root {
             --brand-50: oklch(0.97 0.02 280);
